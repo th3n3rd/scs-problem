41c53a214765c6ca02f2ed86e62bdabef7cb3291
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-02-08 08:10:15 +0000

    Split overrides and nooverrides profiles properly

--- src/test/java/com/example/scsproblem/OverridesProfileTests.java
+++ src/test/java/com/example/scsproblem/OverridesProfileTests.java
@@ -2,5 +2,5 @@ package com.example.scsproblem;
 
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("default")
-class DefaultProfileTests extends ScsProblemTests {}
+@ActiveProfiles("overrides")
+class OverridesProfileTests extends ScsProblemTests {}
